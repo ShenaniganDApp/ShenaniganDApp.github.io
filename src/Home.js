051b85a73dbfad7adfdd85074399c9d0ad145139
YoungKidWarrior <vginelli18@cmc.edu>
2020-01-10 23:54:18 -0800

    add Fixed Backdrop

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div``;
+const Backdrop = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  z-index:-1;
+`;
 const Title = styled.h1`
     color:#e6ffff; 
     text-align:center; 
@@ -9,18 +17,21 @@ const Title = styled.h1`
     font-size: 10rem; 
     font-family: 'Roboto', sans-serif; 
     font-weight: 900;
-    margin:0;"
+    margin:0;
+    
 `;
 const Slogan = styled.p`
     color:#e6ffff;
     text-align: center;
     font-size: 1rem
     font-family: 'Roboto', sans-serif;
+
 `;
 
 function Home() {
   return (
     <Wrapper>
+      <Backdrop />
       <Title>SHE</Title>
       <Slogan>a community-driven, talent support service</Slogan>
     </Wrapper>
