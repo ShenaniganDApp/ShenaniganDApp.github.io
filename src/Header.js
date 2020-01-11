e61ca9a1fbe00cec81622bacce9039fb2f2074ce
YoungKidWarrior <vginelli18@cmc.edu>
2020-01-11 04:05:30 -0800

    Transparent header and responsive spacing

--- src/Header.js
+++ src/Header.js
@@ -2,42 +2,68 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background: #595959;
-  margin: 0;
-  padding: 1rem 0;
-  width: 100%;
   display: flex;
-  justify-content: flex-start;
-`;
+  height:5vw
+  justify-content: flex-start
+  `;
 
 const Logo = styled.img``;
+const Nav = styled.div`
+  margin: 0;
+  height: 5vw;
+  display: flex;
+  justify-content: space-evenly;
+  width:100%
+  &:last-child {
+    border-right: none;
+  }
+`;
+
+const Frame = styled.div`
+  top: 0;
+  height:100%
+  width: 10%;
+  margin-left: 3%;
+  background-color: #ff006c;
+`;
+
 const Tab = styled.div`
-  margin: 0 1.5rem;
+  flex: 1;
+  padding-left: 1em;
+  padding-right: 1em;
   text-align: center;
+  border-right: 1px solid #a29f9fd4
 `;
 
-const Text = styled.div`
+const Text = styled.p`
+  text-decoration: none;
   color: #e6ffff;
+  -webkit-text-stroke: 1px #ff006c;
+  font-weight: 200;
   text-align: center;
-  font-size: 1rem;
+  font-size: 5vw;
   font-family: "Roboto", sans-serif;
+  margin:0;
+  padding:0;
 `;
-
-
 
 function Header() {
   return (
     <Wrapper>
-      <Logo src="./images/logo180.png" />
-      <Tab>
-        <Text>Home</Text>
-      </Tab>
-      <Tab>
-        <Text>Contact</Text>
-      </Tab>
-      <Tab>
-        <Text>About</Text>
-      </Tab>
+      <Frame>
+        <Logo src="images/logo180.png" />
+      </Frame>
+      <Nav>
+        <Tab>
+          <Text>Home</Text>
+        </Tab>
+        <Tab>
+          <Text>Contact</Text>
+        </Tab>
+        <Tab>
+          <Text>About</Text>
+        </Tab>
+      </Nav>
     </Wrapper>
   );
 }
