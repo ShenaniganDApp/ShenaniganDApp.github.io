e285c456adeb559caff16bb3d85b2230c999bc18
YoungKidWarrior <vginelli18@cmc.edu>
2020-01-23 23:02:38 -0800

    Updates

---
+++ precache-manifest.c6cfcdd1418a3bcedde87cf1c3ae75e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61af9b7e9f15a953b66787ced64f04dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f917b63dbfa24fc124d4",
+    "url": "/static/css/main.7668383d.chunk.css"
+  },
+  {
+    "revision": "959344478f83eba19cad",
+    "url": "/static/js/2.1aa5225b.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.1aa5225b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f917b63dbfa24fc124d4",
+    "url": "/static/js/main.fa09f2ac.chunk.js"
+  },
+  {
+    "revision": "81e533c300a6f73e3621",
+    "url": "/static/js/runtime-main.7ae70540.js"
+  },
+  {
+    "revision": "3264e29b3fa14fa9eb73ac942dd83843",
+    "url": "/static/media/background.3264e29b.jpg"
+  }
+]);
