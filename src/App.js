8414d3093273896af76005fc0cf4600a77996e7d
YoungKidWarrior <vginelli18@cmc.edu>
2020-01-25 19:17:55 -0800

    Calculate height of contact and about section

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React from "react";
-import styled from "styled-components";
-import Home from "./Home";
-import Header from "./Header";
-import Contact from "./Contact";
-import About from "./About";
-import Section from "./styles/Section"
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useRef, useState, createRef, useEffect } from 'react';
+import styled from 'styled-components';
+import Home from './Home';
+import Header from './Header';
+import Contact from './Contact';
+import About from './About';
+import Section from './styles/Section';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const AppWrapper = styled.div``;
+
 const MainWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
 `;
 
 function App() {
+  const [heights, setHeights] = useState({});
+  const contactRef = useRef();
+  const aboutRef = useRef();
+
+  useEffect(() => {
+    setHeights({
+      contact: -contactRef.current.boundingTop(),
+      about: -aboutRef.current.boundingTop()
+    });
+  }, [contactRef, aboutRef]);
+
   return (
     <Router>
       <AppWrapper id="top">
-        <Header />
-
+        <Header heights={heights} />
         <Home />
         <MainWrapper>
-          <Contact />
-          <About />
+          <Contact ref={contactRef} />
+          <About ref={aboutRef} />
         </MainWrapper>
       </AppWrapper>
     </Router>
