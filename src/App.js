052856a8cecb21ab6ca321b3ddc9e9a5d34fd3a3
Victor Ginelli <victorginelli@Gilbertos-MacBook-Pro.local>
2020-02-18 20:03:06 -0800

    Apply Background image in virtual dom

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import styled from 'styled-components';
 import { LandingSection, AboutSection } from './Sections';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const AppWrapper = styled.div``;
+const AppWrapper = styled.div`
+  /*Photo by Element5 Digital on Unsplash*/
+  background-image: url(${require('./images/background.jpg')});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: 50% 35%;
+  z-index: -1;
+  @media (max-width: 768px) {
+    body {
+      height: 100vh;
+    }
+  }
+`;
 
 function App() {
   return (
     <Router>
-      <AppWrapper id="top" />
-      <Switch>
-        <Route exact path="/">
-          <LandingSection />
-        </Route>
-        <Route path="/explain">
-          <AboutSection />
-        </Route>
-      </Switch>
+      <AppWrapper id="top">
+        <Switch>
+          <Route exact path="/">
+            <LandingSection />
+          </Route>
+          <Route path="/explain">
+            <AboutSection />
+          </Route>
+        </Switch>
+      </AppWrapper>
     </Router>
   );
 }
