1758815c938db2432d3feb9fdddafbdb0d70dca6
Victor Ginelli <victorginelli@Gilbertos-MacBook-Pro.local>
2020-02-23 20:06:54 -0800

    background position

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const AppWrapper = styled.div`
   @media (max-width: 768px) {
     background-image: url(${require('./images/background_mobile.png')});
     background-repeat: no-repeat;
+    background-position: 0 0,50% 0;
     background-size: auto,auto 30%;
   }
 `;
