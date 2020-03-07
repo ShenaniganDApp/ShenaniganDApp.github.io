35c83dee2ab5fa0c460bb61346265379d47c368a
Victor Ginelli <victorginelli@Gilbertos-MacBook-Pro-2.local>
2020-03-06 22:36:58 -0800

    Energy section styling

--- src/Sections/EnergySection.js
+++ src/Sections/EnergySection.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
   height: auto;
-  width: 85vw;
+  width: 75vw;
   display: flex;
   padding-bottom: 5%;
   justify-content: center;
@@ -23,7 +23,7 @@ const TopLeftSection = styled(Section)`
   border-top-left-radius: 15px;
   border-bottom-left-radius: 15px;
   z-index: 1;
-  width: 55%;
+  width: 60%;
   @media (max-width: 768px) {
     z-index: 0;
     width: 90%;
@@ -35,9 +35,10 @@ const TopLeftSection = styled(Section)`
 `;
 
 const TopRightSection = styled(Section)`
+  width: 40%;
   border-top-right-radius: 15px;
   border-bottom-right-radius: 15px;
-  border: 2vw solid ${colors.lightcyan};
+  /* border: 2vw solid ${colors.lightcyan}; */
   z-index: 0;
   background: rgba(208, 0, 108, 0.7) url(${require('../svg/ethereumLogo.svg')});
   background-size: 60% 60%;
@@ -90,7 +91,7 @@ const SecondBottomSection = styled(Section)`
 
 const UpperTextSection = styled(Section)`
   width: 40%;
-  margin: 1rem -5rem 0rem 1rem;
+  margin: 1rem 0rem 0rem -1rem;
   align-items: flex-start;
 `;
 
@@ -117,11 +118,12 @@ const BoldText = styled(Text)`
 `;
 
 const MockupSection = styled(Section)`
-   margin: 1rem 0rem 1rem -5rem;
+  width: 65%;
+  margin: 1rem 0rem 1rem -8rem;
   @media (max-width: 768px) {
     margin: 1rem 0rem 1rem -2rem;
   }
-`
+`;
 
 const MockupImg = styled.img`
   margin-top: auto;
@@ -191,11 +193,11 @@ function EnergySection(props, ref) {
         margin={'0 0 0 0'}
         backgroundColor={colors.lightcyan}
       >
-        <MockupSection centered width={'60%'}>
+        <MockupSection centered>
           <MockupImg src={require('../images/iphoneMockup.png')} />
         </MockupSection>
         <UpperTextSection>
-          <Section width={'100%'}>
+          <Section textCentered width={'100%'}>
             <Text width={'100%'}>
               <Text width={'100%'} title color={'black'}>
                 What
@@ -221,36 +223,38 @@ function EnergySection(props, ref) {
           </Section>
         </UpperTextSection>
       </TopLeftSection>
-      <TopRightSection
-        shadowed
-        width={'35%'}
-        curved
-        backgroundColor={'rgba(208,0,108,0.7)'}
-      >
+      <TopRightSection shadowed curved backgroundColor={'rgba(208,0,108,0.7)'}>
         <Section margin={'1rem 1rem'}>
           <Text>
             <BoldText size={'3vw'} color={colors.lightcyan}>
               iOns&nbsp;
             </BoldText>
-            <Text shadowed main color={colors.lightcyan}>
-              in nature need both{' '}
-            </Text>
-            <Text shadowed main color={colors.lightcyan}>
-              positive and negative{' '}
-            </Text>
-            <Text shadowed main color={colors.lightcyan}>
-              particles to exist
+            <Text
+              shadowed
+              main
+              margin={'1rem 0 0 1rem'}
+              color={colors.lightcyan}
+            >
+              Natural ions are made up with both positive and negative energy,
+              conjoining to form an energized element as unique as it is
+              necessary
             </Text>
           </Text>
-          <Section centered width={'100%'} margin={'5rem 0 0 0'}>
+          <Section width={'100%'} margin={'2rem 0 0 0'}>
             <BoldText size={'2.7vw'} color={colors.lightcyan}>
               Energy&nbsp;&nbsp;Is&nbsp;&nbsp;
             </BoldText>
             <BoldText size={'2.7vw'} color={colors.lightcyan}>
               Ethereum&nbsp;
             </BoldText>
-            <Text shadowed main color={colors.lightcyan}>
-              Energize yourself to push further as an athlete. iOns are the
+            <Text
+              shadowed
+              main
+              margin={'1rem 0 0 1rem'}
+              color={colors.lightcyan}
+            >
+              Energize yourself to push further as an athlete. iOns get energy
+              from the community to accomplish their goals. You will be the
               center of attention.
             </Text>
           </Section>
