563a7f9af5d0cd6f585874050cf94dc36583db14
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-03 22:56:33 -0800

    Staking components and web3

--- src/Sections/HomeSection.js
+++ src/Sections/HomeSection.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Section, Text, colors } from '../styles';
 import { Transition } from 'react-transition-group';
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
 	display: flex;
@@ -16,6 +17,25 @@ const Backdrop = styled.div`
 	position: absolute;
 	top: 0;
 	z-index: 0;
+`;
+const Button = styled.div`
+	transition: 0.3s;
+	margin: 2rem;
+	padding: 2rem 5rem;
+	box-shadow: rgba(0, 0, 0, 0.9) 0px 10px 20px;
+	border-radius: 15px;
+	border: 2px solid ${colors.deeppink};
+	color: ${colors.lightcyan};
+	text-align: center;
+	display: inline-block;
+	&:hover {
+		transform: scale(0.9);
+		background-color: ${colors.lightcyan};
+		color: ${colors.deeppink};
+	}
+	@media (max-width: 768px) {
+		margin-bottom: 2rem;
+	}
 `;
 const Title = styled.h1`
 	color: ${colors.deeppink};
--- src/Sections/index.js
+++ src/Sections/index.js
@@ -3,19 +3,21 @@ import HomeSection from './HomeSection';
 import EnergySection from './EnergySection';
 import TeamSection from './TeamSection';
 import ContactSection from './ContactSection';
-import LandingSection from './LandingSection'
+import LandingSection from './LandingSection';
 import AboutSection from './AboutSection';
-import QuestionSection from './QuestionSection'
-import EntrySection from './EntrySection'
+import QuestionSection from './QuestionSection';
+import EntrySection from './EntrySection';
+import StakingSection from './StakingSection';
 
 export {
-  MilestoneSection,
-  HomeSection,
-  EnergySection,
-  TeamSection,
-  ContactSection,
-  LandingSection,
-  AboutSection,
-  QuestionSection,
-  EntrySection
+	MilestoneSection,
+	HomeSection,
+	EnergySection,
+	TeamSection,
+	ContactSection,
+	LandingSection,
+	AboutSection,
+	QuestionSection,
+	EntrySection,
+	StakingSection,
 };
