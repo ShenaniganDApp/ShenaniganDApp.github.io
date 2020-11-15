e3549dd260589545a53138a23b187a0080ca1776
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-15 14:47:37 -0800

    fix entry page staking scroll

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import MilestoneContent from './MilestoneContent';
 import DiscordButton from './DiscordButton';
 import StakeButton from './StakeButton';
 import StakeInput from './StakeInput';
+import ScrollToTop from './ScrollToTop';
 
-export { HeaderTab, MilestoneContent, DiscordButton, StakeButton, StakeInput };
+export { HeaderTab, MilestoneContent, DiscordButton, StakeButton, StakeInput, ScrollToTop };
